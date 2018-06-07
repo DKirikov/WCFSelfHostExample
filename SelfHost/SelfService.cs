09cd8ae6a6b222c071293cacf573f2e3cff6acae
DKirikov <KirikovDmitriy@gmail.com>
2018-06-07 16:25:25 +0300

    Initial commit

---
+++ SelfHost/SelfService.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.ServiceModel;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SelfHost
+{
+    public class Test1
+    {
+        public object name;
+    }
+
+    [ServiceContract]
+    public interface ISelfService
+    {
+        int CallsCount
+        {
+            [OperationContract]
+            get;
+        }
+
+        [OperationContract]
+        String SetTestObject(Test1 testObject);
+    }
+
+    [ServiceBehavior(InstanceContextMode = InstanceContextMode.Single, ConcurrencyMode = ConcurrencyMode.Multiple)]
+    public class SelfService : ISelfService
+    {
+        private int callsCount = 0;
+        private string lastName = "none";
+
+        public int CallsCount
+        {
+            get
+            {
+                callsCount++;
+                return callsCount;
+            }
+        }
+
+
+        public String SetTestObject(Test1 testObject)
+        {
+            if (testObject.name is String)
+            {
+                string res = "last name: '" + lastName + "' new name: '" + (testObject.name as string) + "'";
+                lastName = testObject.name as string;
+                return res;
+            }
+
+            throw new Exception("testObject.name is not String");
+        }
+    }
+}
---
+++ WCFClient1/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WCFClient1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Inside Program::Main");
+
+            SelfServiceReference.SelfServiceClient client = new SelfServiceReference.SelfServiceClient();
+
+            try
+            {
+                var callsCount = client.get_CallsCount();
+                Console.WriteLine("CallsCount = " + callsCount.ToString());
+
+                WCFClient1.SelfServiceReference.Test1 testObj = new SelfServiceReference.Test1();
+
+                testObj.name = args.Count() > 0 ? args[0] : "name_" + new Random().Next(100).ToString();
+                string returnValue = client.SetTestObject(testObj);
+                Console.WriteLine("Return value = " + returnValue);
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("Error! Remote service doesn't respond!");
+            }
+
+            Console.ReadKey();
+            client.Close();
+        }
+    }
+}
